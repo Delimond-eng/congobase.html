671449d48370fa3b29962fdec06b25ff28847b26
unknown <Delimond-eng>
2024-05-29 01:07:32 +0200

    upgrade

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
+cd
 git init
 git add -A
 git commit -m 'deploy'
@@ -11,4 +11,5 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
-git push -f git@github.com:Delimond-eng/hrnew.git master:gh-pages
+git push -f git@github.com:Delimond-eng/congobase.html.git master:gh-pages
+cd
